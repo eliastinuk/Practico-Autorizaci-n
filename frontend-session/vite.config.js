913d80eeff845446f06d5fc52271e04997a263f3
unknown <eliastinuk@gmail.com>
2024-09-05 14:56:52 -0300

    Se terminó de modularizar el trabajo práctico

--- frontend-session/vite.config.js
+++ frontend-session/vite.config.js
@@ -1,9 +1,9 @@
-import { defineConfig} from 'vite';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-    server:{
-        port: 3000,
-    open: "/index.html"
+    server: {
+        port: 3000,  // El puerto 3000 se especifica directamente como un número
+        open: '/index.html',  // Configura Vite para abrir automáticamente /index.html al iniciar el servidor
     },
     root: './',
     base: '/',
@@ -13,4 +13,4 @@ export default defineConfig({
         assetsDir: 'assets',
     },
     plugins: [],
-})
+});
